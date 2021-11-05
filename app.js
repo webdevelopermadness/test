94293ca9bfb7502e7ecf6ed78094448e066b24ef
yahya <yahya_1357@outlook.com>
2021-11-05 21:50:42 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// var  a = "hello world";
+
+// function b(){
+//     // console.log('called b!');
+// }
+// // b();
+// // console.log(a);
+
+// console.log(libraryName);
+
+//closure 
+
+// function greet(whattosay){
+//     return function(name){
+//         console.log(whattosay + " " + name);
+//     }
+// }
+
+// // greet("Hi")("tony");
+
+// var sayHi = greet("Hi");
+// sayHi("Tony");

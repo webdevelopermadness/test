3d1d94438840d0faab1bd1ab0957fb3db3136308
yahya <yahya_1357@outlook.com>
2021-11-05 21:55:04 +0500

    first commit

---
+++ lib1.js
@@ -0,0 +1,19 @@
+// var libraryName = "library 1";
+// function greet() {
+//     console.log('hi');
+// }
+// greet.language = "English";
+// console.log(greet.language);
+
+// function greet(){
+//     console.log('hi');
+// }
+// greet();
+
+// function log(a){
+//     a();
+// }
+
+// log(function(){
+//     console.log("hi");
+// });
